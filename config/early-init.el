0d9ee4618a9dda6272adf137e4dd57d7b8a7c96a
psyclyx <me@psyclyx.xyz>
2025-07-27 15:49:15 -0700

    perf: drop gc-cons-threshold even lower

--- config/early-init.el
+++ config/early-init.el
@@ -1,6 +1,6 @@
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+          (lambda () (setq gc-cons-threshold (* 12 1024 1024))))
 
 (let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
   (push default-directory load-path)
