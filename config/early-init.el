46e921b8150b0e79aa0566296ea8e07532091364
psyclyx <me@psyclyx.xyz>
2025-07-27 09:29:55 -0700

    disable gc during startup

--- config/early-init.el
+++ config/early-init.el
@@ -1,3 +1,7 @@
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+
 (let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
   (push default-directory load-path)
   (normal-top-level-add-subdirs-to-load-path))
