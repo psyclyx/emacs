6dcfae82f2ea66d25c4c5ea94b3f81ec1a551846
psyclyx <me@psyclyx.xyz>
2025-07-27 18:10:07 -0700

    fix: typo in athame-help.el

--- config/lisp/athame-help.el
+++ config/lisp/athame-help.el
@@ -32,7 +32,7 @@
   (which-key-idle-delay 0.3)
   (which-key-idle-secondary-delay 0.1)
   (which-key-allow-evil-operators t)
-  (which-key-ashow-operator-state-maps t)
+  (which-key-show-operator-state-maps t)
   :config
   (which-key-mode)
   (which-key-setup-side-window-bottom))
