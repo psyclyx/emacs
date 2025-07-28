7b396872f3465dec8628be36c019934b342cbf04
psyclyx <me@psyclyx.xyz>
2025-07-28 12:26:04 -0700

    experiment: hide initial frame during init

--- config/init.el
+++ config/init.el
@@ -1,3 +1,4 @@
 ;; temp: remove this once refactoring is done
 (setq use-package-compute-statistics t)
 (require 'athame)
+(run-hooks 'athame-ui-after-init-hook)
--- config/lisp/athame-init.el
+++ config/lisp/athame-init.el
@@ -2,28 +2,36 @@
 ;;; Commentary:
 ;;; Code
 
-(defun athame-start--strip-gui ()
+(defun athame-init--disable-pgtk-delay ()
+  (setq pgtk-wait-for-event-timeout nil))
+
+(defun athame-init--hide-initial-frame ()
+  (unless (daemonp)
+    (push '(visibility . nil) initial-frame-alist)
+    (add-hook 'athame-ui-after-init-hook #'make-frame-visible)))
+
+(defun athame-init--strip-gui ()
   "Quickly disables `menu-bar-mode', `tool-bar-lines', and `scroll-bar-mode'.
 From doom."
-   (push '(menu-bar-lines . 0)   default-frame-alist)
-   (push '(tool-bar-lines . 0)   default-frame-alist)
-   (push '(vertical-scroll-bars) default-frame-alist)
-   (setq menu-bar-mode nil
-         tool-bar-mode nil
-         scroll-bar-mode nil))
+  (push '(menu-bar-lines . 0)   default-frame-alist)
+  (push '(tool-bar-lines . 0)   default-frame-alist)
+  (push '(vertical-scroll-bars) default-frame-alist)
+  (setq menu-bar-mode nil
+        tool-bar-mode nil
+        scroll-bar-mode nil))
 
-(defun athame-start--inhibit-startup-redisplay ()
+(defun athame-init--inhibit-startup-redisplay ()
   "Prevent redisplays during startup.
 From doom."
   (setq-default inhibit-redisplay t
                 inhibit-message t
                 frame-inhibit-implied-resize t)
-  (defun athame--reset-inhibited-vars-h ()
+  (defun athame-init--reset-inhibited-vars-h ()
     (setq-default inhibit-redisplay nil inhibit-message nil)
-    (remove-hook 'post-command-hook #'athame--reset-inhibited-vars-h))
-  (add-hook 'post-command-hook #'athame--reset-inhibited-vars-h -100))
+    (remove-hook 'post-command-hook #'athame-init--reset-inhibited-vars-h))
+  (add-hook 'post-command-hook #'athame-init--reset-inhibited-vars-h -100))
 
-(defun athame-start--simple-startup-screen ()
+(defun athame--init--simple-startup-screen ()
   "Disables the default initial start screen."
   (setq inhibit-startup-screen t
         inhibit-startup-echo-area-message user-login-name ; does this work?
@@ -34,6 +42,8 @@ From doom."
 
 (defun athame-init-ui ()
   "Initialize the UI"
+  (athame-init--disable-pgtk-delay) font-lock-function
+  (athame-init--hide-initial-frame)
   (athame-start--strip-gui)
   (athame-start--inhibit-startup-redisplay)
   (athame-start--simple-startup-screen))
