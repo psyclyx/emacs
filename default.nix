fdf11b41c848de8a44bb26c2f334414d39a3a0de
psyclyx <me@psyclyx.xyz>
2026-03-27 04:51:53 -0700

    refactor: remove duplicate hm module from default.nix
    hm-module.nix is the single source of truth for the home-manager
    module. default.nix now only provides the wrapped package.

--- default.nix
+++ default.nix
@@ -14,20 +14,4 @@ in
     ${emacsWithPackages}/bin/emacs --init-directory=${./config}
   '';
 
-  homeManagerModule =
-    { config, lib, pkgs, ... }:
-    {
-      options.psyclyx-emacs.enable = lib.mkEnableOption "Emacs config";
-      config = lib.mkIf config.psyclyx-emacs.enable {
-        programs.emacs = {
-          enable = true;
-          package = emacs;
-          extraPackages = import ./emacsPackages.nix;
-        };
-        home.file.".config/emacs" = {
-          source = ./config;
-          recursive = true;
-        };
-      };
-    };
 }
